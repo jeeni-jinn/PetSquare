6a749cef32906fb9163fcc37e0c7e4b262d26cd2
Eunjin Kim <t01085631409@gmail.com>
2021-11-22 02:11:47 +0900

    no message

--- main.js
+++ main.js
@@ -91,7 +91,9 @@ app.get('/', (req,res)=>{    //"/"에 get요청이 왔을 때
     
     if(req.session.is_logined == true){
         client.query(`select max(id) from a_post;`, function (err, result, fields){
-            console.log((result[0]))
+            array = {}
+            array = result[0];
+            console.log((array[0]))
             if(result){
                 global.number = result[0] +1;
             }
