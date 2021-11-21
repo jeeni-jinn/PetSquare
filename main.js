629e2e62b55a0155b61b41e7b8fc2c0078840f6e
Eunjin Kim <t01085631409@gmail.com>
2021-11-22 01:36:01 +0900

    게시판 수정

--- main.js
+++ main.js
@@ -91,7 +91,7 @@ app.get('/', (req,res)=>{    //"/"에 get요청이 왔을 때
     
     if(req.session.is_logined == true){
         client.query('select max(id) from a_post;', function (err, result, fields){
-            
+            console.log(result);
             if(result){
                 number = parseInt(result[0])+1;
             }
